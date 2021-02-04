d5d42734465401e0eaad26060ea31414d502092c
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2021-02-03 19:46:35 -0500

    changed msg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ msg: "Hello Node"})
+    res.send({ msg: "Hello Node, and Heroku!"})
 });
 
 const PORT = process.env.PORT || 5000;
